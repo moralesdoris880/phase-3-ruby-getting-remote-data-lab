cf8e64c47b3770ec7c3562967f3605cad75e8e82
Doris Morales <moralesdoris880@gmail.com>
2022-02-25 13:53:35 -0500

    Completed assignment

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,19 @@
 # Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+class GetRequester
+    attr_accessor :url
+    def initialize(url)
+        @url= url
+    end
+    def get_response_body
+        uri=URI.parse(self.url)
+        response= Net::HTTP.get_response(uri)
+        response.body
+    end
+    def parse_json
+        body=self.get_response_body
+        JSON.parse(body)
+    end
+end
